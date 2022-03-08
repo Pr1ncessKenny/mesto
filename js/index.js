5d4096f416ae85bae87bc56ce9f825b090675fe5
Pr1ncessKenny <83363524+Pr1ncessKenny@users.noreply.github.com>
2022-03-09 01:04:32 +0300

    main/Work'sDone

---
+++ js/index.js
@@ -0,0 +1,35 @@
+let popup = document.querySelector(".popup");
+let closePopup = document.querySelector(".popup__close");
+let openPopup = document.querySelector(".profile__settings");
+let profileName = document.querySelector(".profile__name");
+let profilePost = document.querySelector(".profile__subtitle");
+let profileNamePopup = document.querySelector("#name");
+let profilePostPopup = document.querySelector("#who-is");
+let formElement = document.querySelector(".popup__btn");
+let popupForm = document.querySelector(".popup__form");
+
+
+function popupOpen(){
+  popup.classList.add('popup_opened');
+}
+
+function popupClose(){
+  popup.classList.remove('popup_opened');
+}
+
+function addProfileData() {
+  profileNamePopup.value = profileName.textContent;
+  profilePostPopup.value = profilePost.textContent;
+}
+
+function profileNameChange(evt) {
+  evt.preventDefault();
+  profileName.textContent = profileNamePopup.value;
+  profilePost.textContent = profilePostPopup.value;
+  popupClose();
+}
+openPopup.addEventListener('click', () => {
+  addProfileData();
+});
+
+popupForm.addEventListener('submit', profileNameChange);
