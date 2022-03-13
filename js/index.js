0ec97584c5793a840782c6de3088a816c95414e6
Pr1ncessKenny <83363524+Pr1ncessKenny@users.noreply.github.com>
2022-03-13 22:51:18 +0300

    hotfix/css&js

--- js/index.js
+++ js/index.js
@@ -1,39 +1,30 @@
 let popup = document.querySelector(".popup");
-let closePopup = document.querySelector(".popup__close");
-let openPopup = document.querySelector(".profile__settings");
+let popupClose = document.querySelector(".popup__close");
+let popupOpen = document.querySelector(".profile__settings");
 let profileName = document.querySelector(".profile__name");
 let profilePost = document.querySelector(".profile__subtitle");
 let profileNamePopup = document.querySelector("#name");
 let profilePostPopup = document.querySelector("#who-is");
-let formElement = document.querySelector(".popup__btn");
 let popupForm = document.querySelector(".popup__form");
 
 
-function popupOpen(){
+function openPopup(){
   popup.classList.add('popup_opened');
-}
-
-function popupClose(){
-  popup.classList.remove('popup_opened');
-}
-closePopup.addEventListener('click', () => {
-  popupClose();
-})
-
-function addProfileData() {
   profileNamePopup.value = profileName.textContent;
   profilePostPopup.value = profilePost.textContent;
 }
 
-function profileNameChange(evt) {
+function closePopup(){
+  popup.classList.remove('popup_opened');
+}
+
+function changeProfileName(evt) {
   evt.preventDefault();
   profileName.textContent = profileNamePopup.value;
   profilePost.textContent = profilePostPopup.value;
-  popupClose();
+  closePopup();
 }
-openPopup.addEventListener('click', () => {
-  popupOpen();
-  addProfileData();
-});
 
-popupForm.addEventListener('submit', profileNameChange);
+popupOpen.addEventListener('click', openPopup);
+popupClose.addEventListener('click', closePopup);
+popupForm.addEventListener('submit', changeProfileName);
